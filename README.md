981c7acd592e1d914833e6a5b9cd99a2ac7268b8
Kriz Cortes <hire@krizcortes.com>
2017-07-20 22:56:22 -0700

    Updated Bomberman gif, Implemented ReadyDom Library and custom ScrollTo function for Vanilla JS throughout app, removed jQuery and Design dir

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # krizcortes.com
 
 My personal portfolio website
-built using HTML, Bootstrap and Sass
+built using HTML5, some Sassy CSS and Vanilla flavored JavaScript
--- assets/js/sirkortes.js
+++ assets/js/sirkortes.js
@@ -1,16 +1,62 @@
-$(document).ready( function(){
+DomReady.ready(function() { 
+  
+  console.log('dom is ready'); 
 
-  // var dot = $("#dot");
-  var hbtn = $("#hireBtn");
-  var cbtn = $("#contactBtn");
-  var actioncall = $("#actioncall");
+  var hbtn = document.getElementById('hireBtn');
+  var cbtn = document.getElementById('contactBtn');
+  var actioncall = document.getElementById('actionCall');
+  var body = document.getElementsByTagName('body')[0];
 
-  hbtn.on('click', function(){
+  hbtn.onclick = function() {
+    scrollTo( body, actioncall, 1200); 
+  }
+      
+  function scrollTo(scroller, to, duration) {
 
-    $.scrollTo( actioncall, { duration: '0.5s' } );
+      var start = 0,
+          to = to.offsetTop,
+          change = to - start,
+          currentTime = 0,
+          increment = 20;
+          
+      var animateScroll = function(){        
+          currentTime += increment;
+          var val = Math.easeInOutQuad(currentTime, start, change, duration);
+          scroller.scrollTop = val;
+          if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+          }
+      };
+      animateScroll();
+  }
 
-  });
+  //t = current time
+  //b = start value
+  //c = change in value
+  //d = duration
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+  };
 
+
+
+
+  // for stars inclusion
+  // xhr = new XMLHttpRequest();
+  // xhr.open("GET","../img/gif/stars2.svg", false);
+  // // Following line is just to be on the safe side;
+  // // not needed if your server delivers SVG with correct MIME type
+  // xhr.overrideMimeType("image/svg+xml");
+  // xhr.send("");
+  // document.getElementById("stars").appendChild(xhr.responseXML.documentElement);
+
+
+});
+  
+  // for form opening
   // cbtn.on('click', function(){
 
   //   cbtn.html('').css({
@@ -21,5 +67,3 @@ $(document).ready( function(){
   //   });
 
   // });
-
-});
